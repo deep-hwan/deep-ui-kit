8777b59af59b0b24d391ae55e1aa18b94034d6be
hwan <hwan@hwanui-MacBookPro.local>
2023-02-13 03:55:07 +0900

    1.6

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Text, Title } from "./components";
 
 export default function App() {
-  return <Title as="h1">asdasd</Title>;
+  return <Title as="h1">asaasdasd</Title>;
 }
